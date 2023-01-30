74df05e2e601a377da5bcb0fbe9acc78aa8d98cd
Krystal <p.krystal95@gmail.com>
2023-01-30 17:53:25 +0900

    react-dom update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
 import App from './App';
 
-ReactDOM.hydrate(<App />, document.getElementById('root'));
+const history = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={history}>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
